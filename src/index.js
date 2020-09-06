63bcc077e645464aeed79ad9e0bae657bc556ae1
WolfMAN <aria.azadi.pour@protonmail.com>
2020-09-06 10:06:13 +0430

    set some headers

--- src/index.js
+++ src/index.js
@@ -2,11 +2,22 @@ const Koa = require("koa");
 const app = new Koa();
 
 app.use(async (ctx, next) => {
+  ctx.accepts("application/json");
+  ctx.acceptsCharsets("utf-8");
+  ctx.response.type = "application/json";
+  await next();
+});
+
+app.use(async (ctx, next) => {
   const startTime = Date.now();
   await next();
   const delta = Date.now() - startTime;
 
   console.log(`${ctx.response.status} ${ctx.method} ${ctx.url} - ${delta}ms`);
+});
+
+app.on("error", (err, ctx) => {
+  console.error(err);
 });
 
 app.listen(3000);
