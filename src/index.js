ef1ec20a9d0ecb7efa1ea298a152bb83f0d8f012
WolfMAN <aria.azadi.pour@protonmail.com>
2020-09-06 09:38:57 +0430

    use logger

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 const Koa = require("koa");
 const app = new Koa();
 
-app.use(async (ctx) => {
-  ctx.body = "Hello World!";
+app.use(async (ctx, next) => {
+  const startTime = Date.now();
+  await next();
+  const delta = Date.now() - startTime;
+
+  console.log(`${ctx.response.status} ${ctx.method} ${ctx.url} - ${delta}ms`);
 });
 
 app.listen(3000);
